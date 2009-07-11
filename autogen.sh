536a7ba1cc3643c447c1e075934b80233f093eca
Jürgen Hötzel <juergen@archlinux.org>
2009-07-11 19:10:24 +0000

    fix package name

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ AM_INSTALLED_VERSION=$($AUTOMAKE --version | sed -e '2,$ d' -e 's/.* \([0-9]*\.[
 if [ "$AM_INSTALLED_VERSION" != "1.10" \
     -a "$AM_INSTALLED_VERSION" != "1.11" ];then
 	echo
-	echo "You must have automake > 1.10 or 1.11 installed to compile lxde-settings-daemon."
+	echo "You must have automake > 1.10 or 1.11 installed to compile lxsession."
 	echo "Install the appropriate package for your distribution,"
 	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
 	exit 1
