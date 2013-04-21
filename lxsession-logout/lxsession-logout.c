6d52423038e70ef4f9d994ad54834a9dcd88c781
Julien Lavergne <julien.lavergne@gmail.com>
2013-04-21 23:40:41 +0200

    Fix previous commit, by using a smarter way to remove \n

--- lxsession-logout/lxsession-logout.c
+++ lxsession-logout/lxsession-logout.c
@@ -679,8 +679,12 @@ int main(int argc, char * argv[])
         {
             output = "";
         }
+        else
+        {
+            output[strlen ( output ) - 1] = '\0';
+        }
 
-        prompt = g_strdup_printf(_("<b><big>Logout %s %.5s session ?</big></b>"), session_name, output);
+        prompt = g_strdup_printf(_("<b><big>Logout %s %s session ?</big></b>"), session_name, output);
     }
     gtk_label_set_markup(GTK_LABEL(label), prompt);
     gtk_box_pack_start(GTK_BOX(controls), label, FALSE, FALSE, 4);
