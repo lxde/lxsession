d883c5758ec855d84dfef0bd340c03fa7a57175d
Julien Lavergne <julien.lavergne@gmail.com>
2013-06-01 12:51:50 +0200

    Fix autogen.sh by backporting autogen.sh from pcmanfm

--- autogen.sh
+++ autogen.sh
@@ -4,15 +4,18 @@ AC_VERSION=
 AUTOMAKE=${AUTOMAKE:-automake}
 AM_INSTALLED_VERSION=$($AUTOMAKE --version | sed -e '2,$ d' -e 's/.* \([0-9]*\.[0-9]*\).*/\1/')
 
-if [ "$AM_INSTALLED_VERSION" != "1.10" \
-    -a "$AM_INSTALLED_VERSION" != "1.11"\
-    -a "$AM_INSTALLED_VERSION" != "1.12" ];then
+# FIXME: we need a better way for version check later.
+case "$AM_INSTALLED_VERSION" in
+    1.1[0-9])
+	;;
+    *)
 	echo
-	echo "You must have automake > 1.10 or 1.11 installed to compile lxsession."
+	echo "You must have automake 1.10 or newer installed."
 	echo "Install the appropriate package for your distribution,"
 	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
 	exit 1
-fi
+	;;
+esac
 
 set -x
 
@@ -22,7 +25,6 @@ fi
 
 ${ACLOCAL:-aclocal$AM_VERSION} ${ACLOCAL_ARG}
 ${AUTOHEADER:-autoheader$AC_VERSION} --force
-AUTOMAKE=$AUTOMAKE libtoolize -c --automake --force
 AUTOMAKE=$AUTOMAKE intltoolize -c --automake --force
 $AUTOMAKE --add-missing --copy --include-deps
 ${AUTOCONF:-autoconf$AC_VERSION}
